f40383d021371ecc8f52493fa61947fe4dc00c76
suresh140 <43436662+suresh140@users.noreply.github.com>
2022-11-01 18:06:42 +0400

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ app = Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 fig = go.Figure(data=[go.Scatter(x=[1, 2, 3], y=[4, 1, 2])])
+fig.update_layout(showlegend=True)
 fig.update_layout(legend=dict(title_text = "Churn Dashboard", yanchor='top',y=1.1, xanchor="right",x=0.99),title=dict(x=0.5))
 
 
