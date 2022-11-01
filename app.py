0d9e5a7757c4c7aedad78d6a768460fde1859d83
suresh140 <43436662+suresh140@users.noreply.github.com>
2022-11-01 12:45:13 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,36 @@
+
+Suresh Vashdev Sharma <suresharma@eand.com>
+12:44 PM (0 minutes ago)
+to me
+
+import plotly.express as px
+import plotly.graph_objects as go
+import pandas as pd
+import dash
+from dash import dcc
+from dash import html
+from dash import Input
+from dash import Output
+import dash_bootstrap_components as dbc
+from dash_bootstrap_templates import load_figure_template
 from flask import Flask
-from dash import Dash
-import dash_core_components as dcc
-import dash_html_components as html
 
 
 server = Flask(__name__)
 app = dash.Dash(
     __name__,
     server=server,
-    url_base_pathname='/dash'
+    url_base_pathname='/dash/'
 )
 
-app.layout = html.Div(id='dash-container')
+fig = go.Figure(data=[go.Scatter(x=[1, 2, 3], y=[4, 1, 2])])
+app.layout = html.Div([dcc.Graph(figure=fig)])
 
-@server.route("/")
-def my_home():
-    return "Hello"
 
 
 @server.route("/dash")
 def my_dash_app():
     return app.index()
+
+if __name__ == "__main__":
+    app.run(debug=True)
