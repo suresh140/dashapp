ca13d95caf8b64751f53fbb7576f564293c304dd
suresh140 <43436662+suresh140@users.noreply.github.com>
2022-11-01 17:48:56 +0400

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from dash import Dash, dcc, html, Input, Output
 import os
 import plotly.graph_objects as go
 import plotly.express as px
+import pandas as pd
 
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -11,6 +12,9 @@ app = Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 fig = go.Figure(data=[go.Scatter(x=[1, 2, 3], y=[4, 1, 2])])
+fig.update_layout(legend=dict(title_text = "Churn Dashboard", yanchor='top',y=1.1, xanchor="right",x=0.99,title=dict(x=0.5))
+
+
 
 
 app.layout = html.Div([
